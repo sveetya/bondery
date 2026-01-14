82d37c12e9384d5bcc5f57632354f3f913130e0a
Marilokms <42029758+Marilok@users.noreply.github.com>
2026-01-14 17:48:19 -0500

    react type

--- packages/branding/src/react/index.d.ts
+++ packages/branding/src/react/index.d.ts
@@ -1,4 +1,4 @@
-import type { SVGProps } from "react";
-
+import type { JSX, SVGProps } from "react";
+import * as React from "react";
 export declare const BonderyIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
 export declare const BonderyIconWhite: (props: SVGProps<SVGSVGElement>) => JSX.Element;
